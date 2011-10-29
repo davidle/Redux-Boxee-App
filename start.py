185c806d199a0e8210b19447eae9734aae23d38b
David Lewis <david.lewis4@bbc.co.uk>
2011-10-29 12:44:22 +0100

    gui changes

--- start.py
+++ start.py
@@ -1,8 +1,11 @@
+#imports boxee, urllibs
 import mc
 import urllib
 import urllib2
 from urllib2 import Request, urlopen, URLError, HTTPError
 
+#Requests login details from users
+#Returns [username, password]
 def getLoginDetails():
 	mc.HideDialogWait()
 	#Pop up dialogs
@@ -20,78 +23,111 @@ def getLoginDetails():
 	mc.ShowDialogWait()
 	return loginDetails
 		
+#Get a redux cookie (log in)
+#Returns the result of the post
 def getNewCookie(loginDetails):
-	#loginDetails = getLoginDetails()
 	#Set up params
 	params = urllib.urlencode([('username', loginDetails[0]), ('password', loginDetails[1]), ('dologin', "1"), ('submit', "log in")])
 	print "posting:"
 	print params
-	#Return pos result
-	return mc.Http().Post('http://www.bbcredux.com/', params)
+	mc.Http().Post('http://www.bbcredux.com/', params)
+	
 
+
+#Displays the first screen after checking redux login details
 def continueLoading():
 	print 'Continuing to load app'
 	mc.ActivateWindow(14000)
-	replaceList('rss://devapi.bbcredux.com/latest.rss/2m-mp4+mp3')
+	replaceList('rss://devapi.bbcredux.com/latest.rss/2m-mp4+mp3?channel=bbcone')
 	mc.HideDialogWait()
 	
-	
+#Replaces the list in the main part of the screen with an Rss feed from rssUrl 	
 def replaceList(rssUrl):
 	items = mc.GetDirectory(rssUrl)
 	mc.GetActiveWindow().GetList(120).SetItems(items)
 	
-	
+#Search for text put into text box, this pulls the rss feed as each letter is typed
+#Might cause a bit of load...	
 def search(terms):
 	print 'search terms:'
 	print terms
-	if len(terms) > 3:
-		encodedTerms = urllib.urlencode([('sort', 'date'), ('q', terms)])
-		queryUrl = 'rss://devapi.bbcredux.com/search.rss/2m-mp4+mp3?sort=date&q='+ encodedTerms
+
+	itemList = mc.ListItems()
+	item = mc.ListItem( mc.ListItem.MEDIA_UNKNOWN)
+	item.SetLabel("Searching...")
+	itemList.append(item)
+
+	if len(terms) > 1:
+		encodedTerms = urllib.urlencode([('q', terms)])
+		queryUrl = 'rss://devapi.bbcredux.com/search.rss/2m-mp4+mp3?&q='+ encodedTerms
 		replaceList(queryUrl) 
 
-mc.ShowDialogWait()
-#Check redux status
-req = urllib2.Request('http://devapi.bbcredux.com/programme/5668414944669113368/media/m2ts/20111028_064500_bbctwo_pet_squad.ts')
-req.get_method = lambda : 'HEAD'
-try:
-	response = urllib2.urlopen(req)
-	print response.read()
-	print response.code
-except HTTPError, e:
-	print 'Initial check error:'
-	print e.code
-	print e.read()
+#Check if we're authenticated on Redux
+def loginCheck():
+	#Not convinced this is sane, always returns a 401, might need an api addition
+	req = urllib2.Request('http://devapi.bbcredux.com/programme/bbchd/2011-09-18/18-00-00/media/mp4-hi/')
+	req.get_method = lambda : 'HEAD'
+	try:
+		response = urllib2.urlopen(req)
+		print 'Auth check success:'
+		print response.read()
+		print response.code
+		return True
+	except HTTPError, e:
+		print 'Auth check error:'
+		print e.code
+		print e.read()
+		if e.code != 401:
+			return True
+		return False
 	
-	#Load login details from config store
+
+#Start here:
+
+mc.ShowDialogWait()
+
+#Check redux login status
+authResult = loginCheck()
+
+if authResult:
+	#True/logged in - continue
+	continueLoading()
+else:
+	#Not logged in - check store for login details
 	config = mc.GetApp().GetLocalConfig()
 	loginDetails= []
 	reduxName = config.GetValue('ReduxName')
 	reduxPass = config.GetValue('ReduxPass')
 	print 'login:'
 	print reduxName
-	print reduxPass
-	
+
+	#Check details are available
 	if reduxName and reduxPass:
+		#Yes, use them
 		print 'Got details from config store'
 		loginDetails = [reduxName, reduxPass]
 	else:
+		#No ask for them
 		print 'Requesting login details'
 		loginDetails = getLoginDetails()
-	#Login/get a cookie
-	result = getNewCookie(loginDetails)	
-	if result:
-		continueLoading()
-	else:
-		mc.ShowDialogNotification("Unable to login to Redux")
-		mc.GetApp().Close()
-		
-		
-#http = mc.Http()
-#http.Get('http://devapi.bbcredux.com/programme/5668456176355154977/')
 
-#resultCode = mc.Http().GetHttpResponseCode()
-else:
+	#Login/get a cookie
+	getNewCookie(loginDetails)
+	
+	#This bit should check if the login worked
+	#keeps returning a 401 though
+	#authResult2 = loginCheck()
+
+	#if authResult2:
 	continueLoading()
+	'''else:
+		mc.ShowDialogNotification("Unable to login to Redux")
+		config = mc.GetApp().GetLocalConfig()
+		config.Reset('ReduxName')
+		config.Reset('ReduxPass')
+		mc.HideDialogWait()
+		mc.GetApp().Close()
+'''
 	
 	
 	
